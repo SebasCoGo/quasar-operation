65400b905af4c356852309639c368dc71a167742
SebasCoGo <sebascogo@gmail.com>
2022-05-25 18:36:50 -0500

    Configuracion para desplegar en AWS

--- src/main/java/com/quasaroperation/config/SwaggerConfig.java
+++ src/main/java/com/quasaroperation/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.quasaroperation.Config;
+package com.quasaroperation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
